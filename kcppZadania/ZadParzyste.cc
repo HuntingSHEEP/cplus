b55dddddc17e8857693c496d90a7fe894e7a5daf
Owca <example.xom>
2021-04-07 11:02:36 +0200

    update: ZadParzyste.cc

--- kcppZadania/ZadParzyste.cc
+++ kcppZadania/ZadParzyste.cc
@@ -2,20 +2,39 @@
 #include<iostream>
 using namespace std;
 
+void fun1(int a){
+  cout<<"FUNKCJA1"<<endl;
+  if(a%2 == 1){
+    cout<<"jest Nieparzysta"<<endl;
+  }else{
+    cout<<"Parzysta"<<endl;
+  }
+}
+
+void fun2(int a){
+  cout<<"FUNKCJA2"<<endl;
+  const char* b = (a%2 == 0) ? "Parzysta" : "Nieparzysta";
+  cout<< ((a%2 == 0) ? "Parzysta" : "Nieparzysta") <<endl;
+}
+
 int main(){
 
   int a;
   cout<<"Podaj liczbe"<<endl;
   scanf(" %d", &a);
 
-  if(a%2 == 1){
-    cout<<"jest Nieparzysta"<<endl;
-  }else{
-    cout<<"Parzysta"<<endl;
+  int f;
+  cout<<"wybierz funkcję [0|1]"<<endl;
+  scanf(" %d", &f);
+
+  switch (f){
+    case 0:
+      fun1(a);
+      break;
+    case 1:
+      fun2(a);
   }
 
-  const char* b = (a%2 == 0) ? "Parzysta" : "Nieparzysta";
-  cout<< ((a%2 == 0) ? "Parzysta" : "Nieparzysta") <<endl;
 
   return 0;
 }
